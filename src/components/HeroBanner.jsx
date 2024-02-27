390eaf26aa00c0a691d5f8d842ba01fb88cab3b0
optimist19 <oluwasegunbamidele3@gmail.com>
2024-02-27 17:46:52 +0100

    on going

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -5,38 +5,43 @@ import { Link } from "react-router-dom";
 import { urlFor } from "../lib/client";
 
 function HeroBanner({ heroBanner }) {
-//   console.log(heroBanner, "props");
+  console.log(heroBanner, "props");
+  console.log(heroBanner.result, "props");
 
   return (
     <div className="hero-banner-container">
-      <div>
-        <p className="beats-solo">{heroBanner.smallText}</p>
-        <h3>{heroBanner.midText}</h3>
-        <h1>{heroBanner.largeText1}</h1>
-        {/* <img src={urlFor(heroBanner.image.asset._ref).url()} alt="headphones" className="hero-banner-image" />  */}
-        <img
-          src={
-            heroBanner?.image?.asset?._ref &&
-            urlFor(heroBanner.image.asset._ref).url()
-          }
-          alt="headphones"
-          className="hero-banner-image"
-        />
+      {heroBanner?.result.map((detail) => {
+        return (
+          <div key={detail._id}>
+          <p className="beats-solo">{detail.smallText}</p>
+          <h3>{detail.midText}</h3>
+          <h1>{detail.largeText1}</h1>
+          {/* <img src={urlFor(heroBanner.image.asset._ref).url()} alt="headphones" className="hero-banner-image" />  */}
+          <img
+            src={
+              detail?.image?.asset?._ref &&
+              urlFor(detail.image.asset._ref).url()
+            }
+            alt="headphones"
+            className="hero-banner-image"
+          />
 
-        <div>
-          <Link to={`/product/${heroBanner.product}`}>
-            <button type="button">BUTTON TEXT</button>
-          </Link>
+          <div>
+            <Link to={`/product/${detail.product}`}>
+              <button type="button">BUTTON TEXT</button>
+            </Link>
 
-          {/* <Link to={`/product/${heroBanner.product}`}>
+            {/* <Link to={`/product/${heroBanner.product}`}>
 				<button type="button">BUTTON TEXT</button>
 				</Link> */}
-          <div className="desc">
-            <h5>Description</h5>
-            <p>{heroBanner.desc}</p>
+            <div className="desc">
+              <h5>Description</h5>
+              <p>{detail.desc}</p>
+            </div>
           </div>
-        </div>
-      </div>
+          {/* <h1>hgvhgvgjh</h1> */}
+        </div>);
+      })}
     </div>
   );
 }
--- src/components/SmallLayout.jsx
+++ src/components/SmallLayout.jsx
@@ -2,69 +2,22 @@ import { useEffect, useState } from "react";
 import HeroBanner from "./HeroBanner";
 import FooterBanner from "./FooterBanner";
 import Product from "./Product";
+import { sanityData } from "../data";
 
 function SmallLayout() {
+  const [arr, setArr] = useState(sanityData)
   const [data, setData] = useState({
     products: "",
     bannerData: ""
   });
 
-  // console.log(data);
+  // console.log(newArray);
 
-  async function fetchData() {
-    try {
-      const params = new URLSearchParams({
-        query: '*[_type == "product"]'
-      });
-
-      // Fetch products data
-      let productResponse = await fetch(
-        `https://o4vudod7.api.sanity.io/v2024-06-02/data/query/production?${params}`
-      );
-      if (!productResponse.ok) {
-        throw new Error("Failed to fetch product data");
-      }
-      let productData = await productResponse.json();
-      setData((prevState) => ({ ...prevState, products: productData }));
-
-      // Construct URLSearchParams object for banner query
-      const bannerParams = new URLSearchParams({
-        query: '*[_type == "banner"]'
-      });
-
-      // Fetch banner data
-      let bannerResponse = await fetch(
-        `https://o4vudod7.api.sanity.io/v2024-06-02/data/query/production?${bannerParams}`
-      );
-      if (!bannerResponse.ok) {
-        throw new Error("Failed to fetch banner data");
-      }
-      let bannerData = await bannerResponse.json();
-      setData((prevState) => ({ ...prevState, bannerData: bannerData }));
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      // Handle error
-    }
-  }
-
-  // console.log(data && data.bannerData && data.bannerData.result && data.bannerData.result[0]);
-
-  useEffect(() => {
-    fetchData();
-    // getServerSideProps()
-  }, []);
-
-  // console.log(data.bannerData[0])
 
   return (
     <div>
       <HeroBanner
-        heroBanner={
-          data &&
-          data.bannerData &&
-          data.bannerData.result &&
-          data.bannerData.result[0]
-        }
+        heroBanner={arr[1]}
       />
 
       <div className="products-heading">
@@ -72,9 +25,8 @@ function SmallLayout() {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {data &&
-          data.products &&
-          data.products.result.map((product) => {
+        {
+          arr[0].result.map((product) => {
             return <Product key={product?.result?._id} product={product} />;
           })}
       </div>
